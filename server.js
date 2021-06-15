dff752db1ffcd52e69676c51a8250cfe49dd0eb2
granis <granis@gmail.com>
2021-06-15 10:40:34 +0200

    dotenv and db-connection

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(express.json());
 app.use(cors());
 
 //DB config
+const connection_url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v59a2.mongodb.net/instagramDB?retryWrites=true&w=majority`;
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("DB connected");
+});
 
 //api endpoints
 app.get("/", (req, res) => res.status(200).send("hello world"));
